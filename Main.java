6d840ea788a8f128fce38afe04c64ef4d00c11ec
Karsten Juarnek <karsten-juranek@web.de>
2024-02-13 17:02:56 +0100

    erstellen von Streams

--- Main.java
+++ Main.java
@@ -10,16 +10,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.*;
 
-public class Main
-{
+public class Main {
     @FunctionalInterface
-    interface Similator<T>
-    {
+    interface Similator<T> {
         double similate(T O1, T O2);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // 'A' == 'A': 1.0
         // 'A' == 'a': 0.5
         // 'A' == 'B': 0.0
@@ -32,6 +29,10 @@ public class Main
             System.out.println(Siml.similate('A', 'A'));
             System.out.println(Siml.similate('A', 'a'));
             System.out.println(Siml.similate('A', 'B'));
+        
+        * Alternativ auch mit einer BiFunction lösbar
+        BiFunction<Character, Character, Double> Similator = (C1, C2) -> 
+            C1 == C2 ? 1.0 : (Character.toUpperCase(C1) == Character.toUpperCase(C2) ? 0.5 : 0.0);
         }*/
 
         // Initiale Stream-Erzeugung: Datenquelle bereitstellen
@@ -40,47 +41,87 @@ public class Main
             Random R = new Random();
             List<Integer> L = new ArrayList<>(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-            Stream<Integer>
-                // Stream aus vorhandener Datenquelle
-                St1 = L.stream(),
+            /*
+            Liste/Map/Set.stream wird verwendet um eine Abbildung dieser zu erstellen
+            
+            Stream.of wird verwendet um einen leeren Stream mit Daten zu füllen
+            
+            Stream.generate akkzeptiert Supplier, es sollte ein limit angegeben werden, da dies
+            sonst den Memory crashen könnte, da infinite Werte generiert werden.
+            
+            Stream.iterate ist ebenfalls eine Methode einen infinite Stream zu erstellen, dieser
+            sollte auch festgelegt werden. An erster stelle der for Schleife steht der seed, bei dem
+            die Funktion beginnen soll. An zweiter Stelle das Predicate, solange das true ist wird die 
+            Funktion ausgeführt. An dritter Stelle steht die Funktion, mithilfe dieser werden die
+            Streamwerte genergiert.
+            
+            Stream.concat wird verwendet um zwei Streams zu einen gemeinsamen Stream zu verbinden
+            
+            Der Stream.builder wird verwendet, wenn nicht alle Elemente gleichzeitig verfügbar sind
+            oder erst Schrittweise eingefügt werden. addAll(Funktion) / add(einzelne Werte)
+             */
 
-                // Stream aus Direktwerten als Daten
-                St2 = Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
-
-                // Generierter Stream aus Supplier
-                // (hier Lambda mit Referenz auf Instanz statt Klasse)
-                // per R.ints() auch als IntStream statt Stream<Integer>
-                St3 = Stream.generate(() -> R.nextInt()).limit(10), // R::nextInt
-
-                // Generierter Stream aus funktionaler Schleife
-                // (Lambdas, die 'for'-Schleife nachbilden)
-                St4 = Stream.iterate(0, I -> I < 10, J -> J + 1),
-
-                // Konkatenierte Streams
-                St5 = Stream.concat(Stream.of(0, 1, 2, 3, 4),
-                                    Stream.of(5, 6, 7, 8, 9)),
-
-                // Stream-Aufbau (auch explizit per eigenem Builder)
-                St6 = Stream.<Integer>builder()
+            Stream<Integer> Stream1 = L.stream(),
+                    Stream2 = Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+                    // Supplier (R)
+                    Stream3 = Stream.generate(() -> R.nextInt()).limit(10),
+                    Stream4 = Stream.iterate(0, I -> I < 10, J -> J + 1),
+                    Stream5 = Stream.concat(Stream.of(0, 1, 2, 3, 4),
+                            Stream.of(5, 6, 7, 8, 9)),
+                    Stream6 = Stream.<Integer>builder()
                             .add(0).add(1).add(2).add(3).add(4)
                             .add(5).add(6).add(7).add(8).add(9)
-                            .build();
+                            .build(),
+                    Stream7 = Stream.concat(Stream1, Stream2);
 
-            // Terminale Operation 'count' schließt Stream!
-            /*System.out.println(St1.count() + ", " + St2.count() + ", " +
-                               St3.count() + ", " + St4.count() + ", " +
-                               St5.count() + ", " + St6.count());*/
-            //System.out.println(St1.count());
-            // Streams sind jetzt 'konsumiert' und können nicht
-            // nochmals benutzt werden!
+            // Terminale Operation 'count' schließt Stream! Sie können nichtmehr abgerufen werden
+            // mit .count wird die Anzahl aller Stream Elemente gezählt
+            
+/System.out.println("Diversifications of Streams:\t"+Stream1.count() + ", \t" + Stream2.count() + ", \t" +
+                    Stream3.count() + ", \t" + Stream4.count() + ", \t" +
+                    Stream5.count() + ", \t" + Stream6.count() + Stream7.count() + ", \t" +"\n");
 
             // Fragen:
             // 1) Erzeugung eines Streams aus String?
-            // 2) Was passiert bei St3, wenn kein Limit gesetzt wird?
-            // 3) Was passiert, wenn St5 aus St1 und St2 gebildet?
+            List<String> StringList = new ArrayList<>(List.of("World", "!"));
+            //Möglichkeiten:
+            Stream<String> StringStream1 = StringList.stream(),
+                    StringStream2 = Stream.of("Hello", " "),
+                    StringStream3 = Stream.concat(StringStream2, StringStream1);
+
+            // Abspeichern sämtlicher Inhalte des Streams mithilfe der Collections
+            // List<String> StreamOutput1 = StringStream3.collect(Collectors.toList());
+            // System.out.println("Safed in Collections\t"+StreamOutput1);
+
+            // Abspeichern des gesamtenStrings mithilfe von joining Mithilfe einer Variablen
+            String combinedString = StringStream3.collect(Collectors.joining());
+            List<String> StringOutput2 = new ArrayList<>();
+            StringOutput2.add(combinedString);
+            System.out.println("Whole String:\t"+StringOutput2+"\n");
+
+            // Direkte Ausgabe in der Konsole des Streams
+            // System.out.print("StringStream3:\t");
+            // StringStream3.forEach(System.out::print);                
+  
+
+            // 2) Was passiert bei Stream3, wenn kein Limit gesetzt wird?
+            //      Es werden unendlich viele Werte generiert, die den Speicher zum crashen bringen
+
+            // 3) Was passiert, wenn Stream5 aus Stream1 und Stream2 gebildet?
+            //      Es werden beide Streams zusammengeführt, somit steigt der count auf 20
+
             // 4) Wann/Wofür macht Stream-Builder Sinn?
+            //      Der Stream.builder macht Sinn, wenn nicht alle Werte gleichzeitig vorhanden sind,
+            //      oder nachträglich ergänzt werden
+
             // 5) Was passiert mit der Datenquelle? Sind die Daten weg?
+            //      In einen Stream wird nur ein Abbild der Daten erstellt, welche mithilfe der Stream-
+            //      Funktionen bearbeitet werden können. Die ursprünglichen Daten werden beibehalten.
+            //      Nach der einmaligen Ausgabe eines Streams ist dieser nichtmehr verfügbar.
+
             // 6) Was passiert, wenn L im Stream mit Daten beschrieben wird?
+            //      Änderungen an der Liste während des Streams sind nicht erlaubt und führen zu
+            //      einer ConcurrentModificationException.
 
             // Aufgaben:
             // 1a) Stream von engl. Alphabet-Zeichen 'A' bis 'Z' in Reihenfolge
@@ -100,35 +141,38 @@ public class Main
             List<Character> LL = List.of('A', '0', 'B', '1', 'C', '2');
 
             List<Integer>
-                // Zahlen quadrieren
-                L1 = L.stream().map(X -> X * X).toList(),
+            // Zahlen quadrieren
+            L1 = L.stream().map(X -> X * X).toList(),
 
-                // Alle Zahlen doppeln
-                L2 = L.stream().flatMap(X -> Stream.of(X, X)).toList(),
+                    // Alle Zahlen doppeln
+                    L2 = L.stream().flatMap(X -> Stream.of(X, X)).toList(),
 
-                // Gerade Zahlen herausfiltern
-                L3 = L.stream().filter(X -> (X & 1) == 0).toList(),
+                    // Gerade Zahlen herausfiltern
+                    L3 = L.stream().filter(X -> (X & 1) == 0).toList(),
 
-                // Ganzzahlige Quadratzahlen herausfiltern
-                L4 = L.stream()
-                      .filter(X -> { double Y = Math.sqrt(X); return Y == (int)Y; })
-                      .toList();
+                    // Ganzzahlige Quadratzahlen herausfiltern
+                    L4 = L.stream()
+                            .filter(X -> {
+                                double Y = Math.sqrt(X);
+                                return Y == (int) Y;
+                            })
+                            .toList();
 
             List<Character>
-                // Zahlen/Ziffern als Zeichen
-                L5 = L.stream().map(X -> (char) (X + '0')).toList(),
+            // Zahlen/Ziffern als Zeichen
+            L5 = L.stream().map(X -> (char) (X + '0')).toList(),
 
-                // Buchstaben gefiltert
-                L6 = LL.stream().filter(C -> Character.isLetter(C)).toList(); /*Character::isLetter*/
+                    // Buchstaben gefiltert
+                    L6 = LL.stream().filter(C -> Character.isLetter(C)).toList(); /*Character::isLetter*/
 
             double[]
-                // Integer-Zahlen als Double
-                A1 = L.stream().mapToDouble(X -> (double) X).toArray(),
+            // Integer-Zahlen als Double
+            A1 = L.stream().mapToDouble(X -> (double) X).toArray(),
 
-                // Gerade Integers entfernen und als Double
-                A2 = L.stream()
-                      .flatMapToDouble(X -> ((X & 1) == 0) ? DoubleStream.empty() : DoubleStream.of(X))
-                      .toArray();
+                    // Gerade Integers entfernen und als Double
+                    A2 = L.stream()
+                            .flatMapToDouble(X -> ((X & 1) == 0) ? DoubleStream.empty() : DoubleStream.of(X))
+                            .toArray();
 
             /*System.out.println("Quadrierung  : "+L1);
             System.out.println("Doppelung    : "+L2);
@@ -157,17 +201,17 @@ public class Main
             List<Integer> LL = new ArrayList<>();
 
             List<Integer>
-                // Sortierung, Aussortierung Doppelter
-                L1 = L.stream().sorted(Comparator.reverseOrder()).toList(),
-                L2 = Stream.of(1, 2, 3, 2, 3, 3).distinct().toList(),
+            // Sortierung, Aussortierung Doppelter
+            L1 = L.stream().sorted(Comparator.reverseOrder()).toList(),
+                    L2 = Stream.of(1, 2, 3, 2, 3, 3).distinct().toList(),
 
-                // Solange verwenden/verwerfen, bis/wie Bedingung erfüllt
-                L3 = L.stream().takeWhile(X -> X <= 4)/*.map(X -> X+X)*/.toList(),
-                L4 = L.stream().dropWhile(X -> X <= 4)/*.map(X -> X+X)*/.toList(),
+                    // Solange verwenden/verwerfen, bis/wie Bedingung erfüllt
+                    L3 = L.stream().takeWhile(X -> X <= 4)/*.map(X -> X+X)*/.toList(),
+                    L4 = L.stream().dropWhile(X -> X <= 4)/*.map(X -> X+X)*/.toList(),
 
-                // Ausspähen (ohne Änderung des Streams), Überspringen
-                L5 = L.stream().peek(LL::add).toList(),     // X -> LL.add(X)
-                L6 = L.stream().skip(5).toList();
+                    // Ausspähen (ohne Änderung des Streams), Überspringen
+                    L5 = L.stream().peek(LL::add).toList(), // X -> LL.add(X)
+                    L6 = L.stream().skip(5).toList();
 
             /*System.out.println("Sortierung: "+L1);
             System.out.println("Distinktion: "+L2);
@@ -184,18 +228,16 @@ public class Main
 
             // Finden/Matchen
             {
-                Optional<Integer>
-                    O1 = L.stream().findFirst(),    // eigentlich returnFirst/Any
-                    O2 = L.stream().findAny(),
-                    O3 = L.stream().min(Integer::compare),
-                    O4 = L.stream().max(Integer::compare);
+                Optional<Integer> O1 = L.stream().findFirst(), // eigentlich returnFirst/Any
+                        O2 = L.stream().findAny(),
+                        O3 = L.stream().min(Integer::compare),
+                        O4 = L.stream().max(Integer::compare);
                 //System.out.println(O1 + ", " + O2);
                 //System.out.println(O3 + ", " + O4);
 
-                boolean
-                    B1 = L.stream().allMatch(X -> X < 10),
-                    B2 = L.stream().anyMatch(X -> X % 5 == 0),
-                    B3 = L.stream().noneMatch(X -> X + X == X);
+                boolean B1 = L.stream().allMatch(X -> X < 10),
+                        B2 = L.stream().anyMatch(X -> X % 5 == 0),
+                        B3 = L.stream().noneMatch(X -> X + X == X);
                 //System.out.println(B1 + ", " + B2 + ", " + B3);
 
                 //System.out.println();
@@ -209,16 +251,15 @@ public class Main
             // Reduzieren/Aufsammeln
             {
                 // Reduce
-                int Sum = L.stream().reduce(0, (Z, X) -> Z+X);   // Integer::sum
-                Optional<Integer> Summy = L.stream().reduce((Z, X) -> Z+X);
+                int Sum = L.stream().reduce(0, (Z, X) -> Z + X); // Integer::sum
+                Optional<Integer> Summy = L.stream().reduce((Z, X) -> Z + X);
                 //System.out.println("Summen: "+Sum + ", " + Summy);
 
                 String R = L.stream().reduce("", (S, X) -> S + X, String::concat);
                 //System.out.println("Stringbildung: "+R);    // könnte parallelisiert werden
 
                 // Collect
-                Set<Integer> Zet1 = L.stream().collect
-                    (HashSet::new, (Z, X) -> Z.add(X / 2), HashSet::addAll);
+                Set<Integer> Zet1 = L.stream().collect(HashSet::new, (Z, X) -> Z.add(X / 2), HashSet::addAll);
                 Set<Integer> Zet2 = L.stream().collect(Collectors.toUnmodifiableSet());
                 //System.out.println("Mengenbildung: "+Zet1 + ", " + Zet2);
 
@@ -226,7 +267,7 @@ public class Main
                 //System.out.println("Durchschnitt: "+Avrg);
 
                 String T = L.stream().map(X -> "" + X)
-                    .collect(Collectors.joining(", ", "[", "]"));
+                        .collect(Collectors.joining(", ", "[", "]"));
                 //System.out.println("String-Verknüpfung: "+T);
 
                 //System.out.println();
@@ -240,8 +281,7 @@ public class Main
 
             // Beispiel für 'interfering function'
             {
-                List<Integer>
-                    LL = new ArrayList<>(L);
+                List<Integer> LL = new ArrayList<>(L);
                 /*List<Integer>
                     LLL = LL.stream().reduce(LL,        // interfering!
                                              (Li, X) -> { Li.add(X); return Li; },
